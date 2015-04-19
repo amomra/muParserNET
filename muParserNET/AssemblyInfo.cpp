fe0f0630a3ac7cc6b25d12eae21747eb83602ace
Luiz Carlos Viana Melo <amomra@users.noreply.github.com>
2015-04-19 17:50:23 -0300

    Adding license

--- muParserNET/AssemblyInfo.cpp
+++ muParserNET/AssemblyInfo.cpp
@@ -1,3 +1,30 @@
+/*
+muParserNET - muParser library wrapper for .NET Framework
+
+Copyright (c) 2015 Luiz Carlos Viana Melo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+This software contains parts copied from muParser library.
+muParser library - Copyright (C) 2013 Ingo Berg
+*/
+
 #include "stdafx.h"
 
 using namespace System;
@@ -16,7 +43,7 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyConfigurationAttribute(L"")];
 [assembly:AssemblyCompanyAttribute(L"Luiz Carlos Viana Melo")];
 [assembly:AssemblyProductAttribute(L"muParserNET")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright (c) Microsoft 2015")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) 2015 Luiz Carlos Viana Melo")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
 
@@ -31,7 +58,7 @@ using namespace System::Security::Permissions;
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute("1.0.*")];
+[assembly:AssemblyVersionAttribute("1.0.0.0")];
 
 [assembly:ComVisible(false)];
 
